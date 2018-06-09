40e9dd7c89e352b6e51e43c0b6555ea85e338072
Matthew Turner <mdtdev@users.noreply.github.com>
2018-06-09 12:14:04 -0400

    Translations to other Formats...

---
+++ DECODE/01_introduction_to_jupyter_and_repl.r
@@ -0,0 +1,18 @@
+
+2+2
+
+options(repr.plot.width=5, repr.plot.height=4) # Set plot height and width
+x = seq(-pi, pi, by=0.01)
+plot(x, sin(x), type="l", col="red", lwd = 3)
+lines(x, cos(x), type="l", col="purple", lwd=3)
+legend(0, -0.5, legend=c("Sine", "Cosine"), col=c("red", "purple"), lty=1,
+       y.intersp=2, cex=0.75, box.lty = 0, lwd=3)
+
+1 + 2
+10*33
+14 - 7
+1/2
+
+a = 10
+
+a + 2
